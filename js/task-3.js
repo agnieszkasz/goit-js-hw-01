24b7a5e74a7ebbe9a0242f998b44763759671c05
Agnieszka Szczepanik <agnieszkaszczepanik93@gmail.com>
2024-04-18 10:30:06 +0200

    alltasksdone

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,6 @@
 function getElementWidth(content, padding, border) {
-  console.log(Number.parseFloat(content + 2 * padding + 2 * border));
+  return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 }
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
